8145bc45f4b4338e3465bf667344a3f7c5702a98
ZeAntonioM <3012005+ZeAntonioM@users.noreply.github.com>
2024-02-27 10:46:25 +0000

    removed target stuff

---
+++ proj/target/release/deps/proj-af3decb0869543e9.d
@@ -0,0 +1,5 @@
+/home/zepc/Desktop/School/6Sem/CPD/CPD-project1/proj/target/release/deps/proj-af3decb0869543e9: src/main.rs
+
+/home/zepc/Desktop/School/6Sem/CPD/CPD-project1/proj/target/release/deps/proj-af3decb0869543e9.d: src/main.rs
+
+src/main.rs:
